cbaa545f80b5536ee4e3484a9d5635a3f67b16b4
Rashi Singh <rashi1912singh@gmail.com>
2026-06-05 14:12:23 +0530

    Add Smart Clean-Up Initiative feature

--- frontend/src/pages/DetectPage.jsx
+++ frontend/src/pages/DetectPage.jsx
@@ -19,7 +19,7 @@ export default function DetectPage() {
             const formData = new FormData();
             formData.append('file', image);
 
-            const response = await fetch('http://127.0.0.1:5000/predict', {
+            const response = await fetch('https://recyclink-77tg.onrender.com/predict', {
                 method: 'POST',
                 body: formData
             });
